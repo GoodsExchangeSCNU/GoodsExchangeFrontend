47654bf06b9a05287e70e8fa87765a25ce488edc
Hongyu Yan <20223803065@m.scnu.edu.cn>
2024-11-04 16:53:35 +0800

    Profile Page completed, Except image loading

--- src/components/profile/TableInfo.vue
+++ src/components/profile/TableInfo.vue
@@ -5,6 +5,8 @@ import router from "@/router/index.js";
 import axios from "@/axios_client/index.js";
 
 import FormatObject from "@/utils/format.js";
+import StateIcon from "@/components/profile/StateIcon.vue";
+import {Picture} from "@element-plus/icons-vue";
 
 // 组件全局属性事件定义
 const props = defineProps({
@@ -232,49 +234,76 @@ onMounted(() => {
 <template>
   <el-table :data="tableData" stripe border style="width: 100%" scrollbar-always-on>
     <el-table-column fixed prop="name" :label="t('saleInfo.name_col_table')" width="100" sortable/>
-    <el-table-column prop="id" :label="t('saleInfo.id_col_table')" width="100" sortable/>
-    <el-table-column prop="picture" :label="t('saleInfo.picture_col_table')" width="200" sortable/>
+    <el-table-column prop="picture" :label="t('saleInfo.picture_col_table')" width="200" sortable>
+      <template #default="scope">
+        <div class="image-preview">
+          <el-image
+              style="width: 100px; height: 100px"
+              :src="scope.row.picture"
+              :zoom-rate="1.2"
+              :max-scale="7"
+              :min-scale="0.2"
+              :preview-src-list="scope.row.picture_list"
+              :initial-index="0"
+              fit="cover"
+          >
+            <template #error>
+              <div class="image-slot">
+                <el-icon><Picture /></el-icon>
+                <div>{{t("saleInfo.picture_load_failed")}}</div>
+              </div>
+            </template>
+          </el-image>
+        </div>
+      </template>
+
+    </el-table-column>
     <el-table-column prop="price" :label="t('saleInfo.price_col_table')" width="100" sortable/>
     <el-table-column prop="buyer" :label="t('saleInfo.buyer_col_table')" width="100" sortable/>
     <el-table-column prop="seller" :label="t('saleInfo.seller_col_table')" width="100" sortable/>
-    <el-table-column prop="state" :label="t('saleInfo.state_col_table')" width="150" sortable/>
+    <el-table-column prop="id" :label="t('saleInfo.id_col_table')" width="100" sortable/>
+    <el-table-column fixed="right" prop="state" :label="t('saleInfo.state_col_table')" width="150" sortable>
+      <template #default="scope">
+        <StateIcon :state="scope.row.state" />
+      </template>
+    </el-table-column>
     <el-table-column fixed="right" prop="operation" :label="t('saleInfo.operation_col_table')" width="300" sortable>
       <template #default="scope">
         <template v-if="scope.row.state === 0">
           <div>{{t("sellInfo.event_operation_None")}}</div>
         </template>
         <template v-if="(scope.row.state === 1) && (props.isSell)">
-          <el-button @click="event_change_reject(scope.row.id)">{{t("saleInfo.event_operation_reject")}}</el-button>
+          <el-button @click="event_change_reject(scope.row.id)" type="danger">{{t("saleInfo.event_operation_reject")}}</el-button>
         </template>
         <template v-if="(scope.row.state === 1) && (!props.isSell)">
-          <el-button @click="event_change_purchase(scope.row.id)">{{t("saleInfo.event_operation_purchase")}}</el-button>
-          <el-button @click="event_change_withdrawn(scope.row.id)">{{t("saleInfo.event_operation_withdrawn")}}</el-button>
+          <el-button @click="event_change_purchase(scope.row.id)" type="success">{{t("saleInfo.event_operation_purchase")}}</el-button>
+          <el-button @click="event_change_withdrawn(scope.row.id)" type="info">{{t("saleInfo.event_operation_withdrawn")}}</el-button>
         </template>
         <template v-if="(scope.row.state === 2) && (props.isSell)">
-          <el-button @click="event_change_reject(scope.row.id)">{{t("saleInfo.event_operation_reject")}}</el-button>
-          <el-button @click="event_change_agree(scope.row.id)">{{t("saleInfo.event_operation_agree")}}</el-button>
+          <el-button @click="event_change_reject(scope.row.id)" type="danger">{{t("saleInfo.event_operation_reject")}}</el-button>
+          <el-button @click="event_change_agree(scope.row.id)" type="success">{{t("saleInfo.event_operation_agree")}}</el-button>
         </template>
         <template v-if="(scope.row.state === 2) && (!props.isSell)">
-          <el-button @click="event_change_withdrawn(scope.row.id)">{{t("saleInfo.event_operation_withdrawn")}}</el-button>
+          <el-button @click="event_change_withdrawn(scope.row.id)" type="info">{{t("saleInfo.event_operation_withdrawn")}}</el-button>
         </template>
         <template v-if="(scope.row.state === 3) && (props.isSell)">
-          <el-button @click="event_change_shipped(scope.row.id)">{{t("saleInfo.event_operation_shipped")}}</el-button>
+          <el-button @click="event_change_shipped(scope.row.id)" type="primary">{{t("saleInfo.event_operation_shipped")}}</el-button>
         </template>
         <template v-if="(scope.row.state === 3) && (!props.isSell)">
-          <div>{{t("sellInfo.event_operation_None")}}</div>
+          <div>{{t("saleInfo.event_operation_None")}}</div>
         </template>
         <template v-if="scope.row.state === 4">
-          <div>{{t("sellInfo.event_operation_None")}}</div>
+          <div>{{t("saleInfo.event_operation_None")}}</div>
         </template>
         <template v-if="scope.row.state === 5">
-          <div>{{t("sellInfo.event_operation_None")}}</div>
+          <div>{{t("saleInfo.event_operation_None")}}</div>
         </template>
         <template v-if="(scope.row.state === 6) && (props.isSell)">
-          <div>{{t("sellInfo.event_operation_None")}}</div>
+          <div>{{t("saleInfo.event_operation_None")}}</div>
         </template>
         <template v-if="(scope.row.state === 6) && (!props.isSell)">
-          <el-button @click="event_change_not_received(scope.row.id)">{{t("saleInfo.event_operation_not_received")}}</el-button>
-          <el-button @click="event_change_completed(scope.row.id)">{{t("saleInfo.event_operation_received")}}</el-button>
+          <el-button @click="event_change_not_received(scope.row.id)" type="danger">{{t("saleInfo.event_operation_not_received")}}</el-button>
+          <el-button @click="event_change_completed(scope.row.id)" type="success">{{t("saleInfo.event_operation_received")}}</el-button>
         </template>
       </template>
     </el-table-column>
--- src/utils/format.js
+++ src/utils/format.js
@@ -4,12 +4,14 @@ const FormatObject = {
     formattedRecordList = recordList.map(entry => ({
       id: entry.id,
       name: entry.item.name,
-      picture: entry.item.img,
+      picture: decodeURIComponent(entry.item.img[0]),
       price: entry.item.price,
       buyer: entry.buyer.username,
       seller: entry.seller.username,
-      state: entry.state
+      state: entry.state,
+      picture_list: entry.item.img.map((img) => decodeURIComponent(img))
     }));
+    console.log(formattedRecordList)
     return formattedRecordList;
   }
 }
